1f6fb33b6e001e44e8e9898332114ef9275c29c5
stzups <lemonpickles1558@gmail.com>
2021-05-21 18:25:09 -0400

    add test unit tests

---
+++ src/test/java/net/stzups/config/ConfigTest.java
@@ -0,0 +1,12 @@
+package net.stzups.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConfigTest {
+    @Test
+    public void testExample() {
+        new Config();
+        Assertions.assertEquals(1, 1);
+    }
+}
